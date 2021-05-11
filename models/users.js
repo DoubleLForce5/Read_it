0a5eb2f929e0b0c41e59b225909fd0fd4a568d95
Ryan Owens <rtowens11@gmail.com>
2021-05-10 21:03:54 -0500

    added first model and db connection config

---
+++ models/users.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Users extends Model {}
+
+Users.init(
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name_first: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name_last: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'users',
+  }
+);
+
+module.exports = Users;
