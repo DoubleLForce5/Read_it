8c8955164cf05eb04b8314110733d4f337ba5b1c
Ryan Owens <rtowens11@gmail.com>
2021-05-12 18:59:13 -0500

    added books model

---
+++ models/books.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Books extends Model {}
+
+Books.init(
+  {
+    book_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    isbn: {
+      type: DataTypes.NUMBER,
+      allowNull: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    year: {
+      type: DataTypes.NUMBER,
+      allowNull: true,
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    author_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'books',
+  }
+);
+
+module.exports = Books;
