977e87fc6b0d4eac57782df0362da73a975e2591
Liv <lopez.olivia17@outlook.com>
2021-05-11 20:04:21 -0500

    modified api.index.js, modified controllers index.js, created routes.js, utils/auth.js, and views/login.hbs

---
+++ Controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const authorsRoutes = require('./authorsRoutes');
+const booksRoutes = require('./booksRoutes');
+const myListRoutes = require('./myListRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/users', userRoutes);
+router.use('/myListRoutes', myListRoutes);
+router.use('/books', booksRoutes);
+router.use('/authors', authorsRoutes);
+
+module.export = router;
