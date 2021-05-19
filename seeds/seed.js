cb3a768bf763b6913ec88f1a29de0161f74a476c
Liv <lopez.olivia17@outlook.com>
2021-05-19 12:41:05 -0500

    addes hasRead, isReading, and willRead seeds data and modified seed.js to reflect new data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,33 +1,45 @@
 const sequelize = require('../config/connection');
-const { Authors, Books, My_List, Users } = require('../models');
+const { Authors, Books, Users, HasRead, IsReading, WillRead } = require('../models');
 
 const authorsSeedData = require('./authorsSeedData.json');
 const booksSeedData = require('./booksSeedData.json');
-const myListSeedData = require('./myListSeedData.json');
+const hasReadSeedData = require('./hasReadSeedData.json');
 const usersSeedData = require('./usersSeedData.json');
+const isReadingSeedData = require('./isReadingSeedData.json');
+const willReadSeedData = require('./willReadSeedData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const authors = await Authors.bulkCreate(authorsSeedData, {
+  await Authors.bulkCreate(authorsSeedData, {
     individualHooks: true,
     returning: true,
   });
 
-  const books = await Books.bulkCreate(booksSeedData, {
+  await Books.bulkCreate(booksSeedData, {
     individualHooks: true,
     returning: true,
   });
 
-  const users = await Users.bulkCreate(usersSeedData, {
+  await Users.bulkCreate(usersSeedData, {
     individualHooks: true,
     returning: true,
   });
 
-  const myList = await My_List.bulkCreate(myListSeedData, {
+  await HasRead.bulkCreate(hasReadSeedData, {
     individualHooks: true,
     returning: true,
   });
+
+  await IsReading.bulkCreate(isReadingSeedData, {
+    individualHooks: true,
+    returning: true,
+  })
+
+  await WillRead.bulkCreate(willReadSeedData, {
+    individualHooks: true,
+    returning: true,
+  })
 
   process.exit(0);
 };
